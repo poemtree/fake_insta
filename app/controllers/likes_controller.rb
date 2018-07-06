9074ac801dcb5e3fcc9dc36e704eb387db816931
poemtree <yym1232@gmail.com>
2018-07-06 06:10:40 +0000

    ajax로 좋아요 기능 구현

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,20 @@
 class LikesController < ApplicationController
   def create
-    Like.create(user_id: current_user.id, post_id: params[:post_id])
-    redirect_to '/'
+    @post_id = params[:post_id]
+    Like.create(user_id: current_user.id, post_id: @post_id)
+    respond_to do |format|
+      format.html {redirect_to :back}
+      format.js {}
+    end
   end
 
   def destroy
-    Like.find_by(user_id: current_user.id, post_id: params[:post_id]).destroy
-    redirect_to '/'
+    @post_id = params[:post_id]
+    Like.find_by(user_id: current_user.id, post_id: @post_id).destroy
+    respond_to do |format|
+      format.html {redirect_to :back}
+      format.js {}
+    end
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   resources :posts
   post '/posts/:post_id/comments' => 'comments#create'
   delete '/comments/:comment_id' => 'comments#destroy', as: 'destroy_comment'
-  get '/posts/:post_id/like' => 'likes#create'
-  get '/posts/:post_id/dislike' => 'likes#destroy'
+  put '/posts/:post_id/like' => 'likes#create'
+  delete '/posts/:post_id/dislike' => 'likes#destroy'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
