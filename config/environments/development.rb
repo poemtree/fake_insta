83bab875803973e71971edf034b87ac7db3e9b48
poemtree <yym1232@gmail.com>
2018-07-18 15:41:17 +0900

    devise와 development에 내용 수정

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,9 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_option = {host: 'http://localhost', port: 3000}
+  config.action_mailer.smtp_settings = {address: ENV["SES_ADDRESS"], user_name: ENV["SES_USER_NAME"], password: ENV["SES_PASSWORD"], port: 587, authentication: :login}
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
